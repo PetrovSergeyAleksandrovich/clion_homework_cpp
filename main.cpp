a9ef5dc1ae72e81af2e3214eec6b7f1b50f6ff15
PetrovSergeyAleksandrovich <ps@dsys.ru>
2020-12-03 12:38:00 +0300

    tmp_commit

--- main.cpp
+++ main.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 
 int main() {
-    int x;
-    std::cout << "Type the number:";
+    int x, y;
+    std::cout << "Type the number x:";
     std::cin >> x;
+    std::cout << "Type the number y:";
+    std::cin >> y;
 
-    for (int i = 0; i < x; i++)
+    for (int i = 0; i < 1; i++)
     {
-        std::cout << "Fuck, World!" << "\n";
+        std::cout << "x = " << x << " " << "y = " << y << "\n";
     }
 
     return 0;
