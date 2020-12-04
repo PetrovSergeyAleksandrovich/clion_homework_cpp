9c0c3868a2727eaa9488327dabb6343a95c12f91
PetrovSergeyAleksandrovich <ps@dsys.ru>
2020-12-04 17:45:03 +0300

    18_6_task_2 DONE

--- HW_18/18_6_task_2.cpp
+++ HW_18/18_6_task_2.cpp
@@ -3,7 +3,30 @@
 
 int main() {
 
-    std::vector <int> vec =  {-2,1,-3,4,-1,2,1,-5,4};
+    std::vector <int> vec = {2, 7, 11, 15};
+    int x = 9; // searching number
+    int number_one, number_two;
+    bool found = false;
+
+    for (int i = 0; i < vec.size()-1; i++)
+    {
+        int sum=0;
+        for (int j = i+1; j < vec.size(); j++)
+        {
+            sum = vec[i] + vec[j];
+            if (sum == x)
+            {
+                number_one = vec[i];
+                number_two = vec[j];
+                std::cout << number_one << " and " << number_two << "\n" << std::endl;
+                found = true;
+                break;
+            }
+        }
+        if (found) break;
+    }
+
+    return 0;
 
     return 0;
 }
